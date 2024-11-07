ae10ebb4100ec9c167dc50962bc395ec7f6e222d
zeusopera9 <zaidalimerchant@yahoo.com>
2024-11-07 16:01:31 +0530

    Minor Changes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 // Configure MySQL connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
